073c1a2becb51b85f74001b97a036fe81250cd1d
helpmehelpus <adrianortorres@gmail.com>
2024-11-02 13:03:23 +1030

    use single environment, use anyhow

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -3,13 +3,13 @@ use std::collections::HashMap;
 use crate::ir::ast::Expression;
 use crate::ir::ast::Name;
 use crate::ir::ast::Statement;
+use anyhow::{bail, Result};
 
 type IntValue = i32;
-type ErrorMessage = String;
 
 type Environment = HashMap<Name, i32>;
 
-pub fn eval(exp: &Expression, env: &Environment) -> Result<IntValue, ErrorMessage> {
+pub fn eval(exp: &Expression, env: &Environment) -> Result<IntValue> {
     match exp {
         Expression::CInt(v) => Ok(*v),
         Expression::Add(lhs, rhs) => Ok(eval(lhs, env)? + eval(rhs, env)?),
@@ -18,39 +18,52 @@ pub fn eval(exp: &Expression, env: &Environment) -> Result<IntValue, ErrorMessag
         Expression::Div(lhs, rhs) => Ok(eval(lhs, env)? / eval(rhs, env)?),
         Expression::Var(name) => match env.get(name) {
             Some(&value) => Ok(value),
-            None => Err(format!("Variable {} not found", name)),
+            None => bail!("Variable {} not found", name),
         },
     }
 }
 
-pub fn execute(stmt: &Statement, env: Environment) -> Result<Environment, ErrorMessage> {
+/// Executes statements. Mutates the environment as it executes. Yes, the "fully" functional approach
+/// is to have total immutability. Note, however, that we are doing things like:
+/// Statement::Sequence(s1, s2) => {
+/// Statement::While(cond, stmt) => {
+///     let mut value = eval(cond, &env)?;
+///     let mut new_env = env;
+///     while value > 0 {
+///     new_env = execute(stmt, new_env.clone())?;
+///    value = eval(cond, &new_env.clone())?;
+///     }
+///     Ok(new_env)
+/// }
+/// which already introduces mutability and creeps .clone() calls everywhere
+pub fn execute(stmt: &Statement, env: &mut Environment) -> Result<()> {
     match stmt {
         Statement::Assignment(name, exp) => {
-            let value = eval(exp, &env)?;
-            let mut new_env = env;
-            new_env.insert(*name.clone(), value);
-            Ok(new_env.clone())
+            let value = eval(exp, env)?;
+            env.insert(*name.clone(), value);
         }
         Statement::IfThenElse(cond, stmt_then, stmt_else) => {
-            let value = eval(cond, &env)?;
+            let value = eval(cond, env)?;
             if value > 0 {
-                execute(stmt_then, env)
+                execute(stmt_then, env)?
             } else {
-                execute(stmt_else, env)
+                execute(stmt_else, env)?
             }
         }
         Statement::While(cond, stmt) => {
-            let mut value = eval(cond, &env)?;
-            let mut new_env = env;
+            let mut value = eval(cond, env)?;
             while value > 0 {
-                new_env = execute(stmt, new_env.clone())?;
-                value = eval(cond, &new_env.clone())?;
+                execute(stmt, env)?;
+                value = eval(cond, env)?;
             }
-            Ok(new_env)
         }
-        Statement::Sequence(s1, s2) => execute(s1, env).and_then(|new_env| execute(s2, new_env)),
-        _ => Err(String::from("not implemented yet")),
+        Statement::Sequence(s1, s2) => {
+            execute(s1, env)?;
+            execute(s2, env)?;
+        },
+        _ => bail!("not implemented yet"),
     }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -58,103 +71,123 @@ mod tests {
     use super::*;
 
     #[test]
-    fn eval_constant() {
+    fn eval_constant() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(10);
         let c20 = Expression::CInt(20);
 
-        assert_eq!(eval(&c10, &env), Ok(10));
-        assert_eq!(eval(&c20, &env), Ok(20));
+        assert_eq!(eval(&c10, &env)?, 10);
+        assert_eq!(eval(&c20, &env)?, 20);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_add_expression1() {
+    fn eval_add_expression1() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(10);
         let c20 = Expression::CInt(20);
         let add1 = Expression::Add(Box::new(c10), Box::new(c20));
-        assert_eq!(eval(&add1, &env), Ok(30));
+        assert_eq!(eval(&add1, &env)?, 30);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_add_expression2() {
+    fn eval_add_expression2() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(10);
         let c20 = Expression::CInt(20);
         let c30 = Expression::CInt(30);
         let add1 = Expression::Add(Box::new(c10), Box::new(c20));
         let add2 = Expression::Add(Box::new(add1), Box::new(c30));
-        assert_eq!(eval(&add2, &env), Ok(60));
+        assert_eq!(eval(&add2, &env)?, 60);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_mul_expression() {
+    fn eval_mul_expression() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(10);
         let c20 = Expression::CInt(20);
         let mul1 = Expression::Mul(Box::new(c10), Box::new(c20));
-        assert_eq!(eval(&mul1, &env), Ok(200));
+        assert_eq!(eval(&mul1, &env)?, 200);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_variable() {
+    fn eval_variable() -> Result<()> {
         let env = HashMap::from([(String::from("x"), 10), (String::from("y"), 20)]);
         let v1 = Expression::Var(String::from("x"));
         let v2 = Expression::Var(String::from("y"));
-        assert_eq!(eval(&v1, &env), Ok(10));
-        assert_eq!(eval(&v2, &env), Ok(20));
+        assert_eq!(eval(&v1, &env)?, 10);
+        assert_eq!(eval(&v2, &env)?, 20);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_sub_expression1() {
+    fn eval_sub_expression1() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(10);
         let c20 = Expression::CInt(20);
         let mul1 = Expression::Sub(Box::new(c20), Box::new(c10));
-        assert_eq!(eval(&mul1, &env), Ok(10));
+        assert_eq!(eval(&mul1, &env)?, 10);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_sub_expression2() {
+    fn eval_sub_expression2() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(100);
         let c20 = Expression::CInt(300);
         let mul1 = Expression::Sub(Box::new(c20), Box::new(c10));
-        assert_eq!(eval(&mul1, &env), Ok(200));
+        assert_eq!(eval(&mul1, &env)?, 200);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_div_expression1() {
+    fn eval_div_expression1() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(10);
         let c20 = Expression::CInt(20);
         let mul1 = Expression::Div(Box::new(c20), Box::new(c10));
-        assert_eq!(eval(&mul1, &env), Ok(2));
+        assert_eq!(eval(&mul1, &env)?, 2);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_div_expression2() {
+    fn eval_div_expression2() -> Result<()> {
         let env = HashMap::new();
         let c10 = Expression::CInt(3);
         let c20 = Expression::CInt(21);
         let mul1 = Expression::Div(Box::new(c20), Box::new(c10));
-        assert_eq!(eval(&mul1, &env), Ok(7));
+        assert_eq!(eval(&mul1, &env)?, 7);
+
+        Ok(())
     }
 
     #[test]
-    fn execute_assignment() {
-        let env = HashMap::new();
+    fn execute_assignment() -> Result<()> {
+        let mut env = HashMap::new();
         let assign_stmt =
             Statement::Assignment(Box::from(String::from("x")), Box::new(Expression::CInt(42)));
 
-        match execute(&assign_stmt, env) {
-            Ok(new_env) => assert_eq!(new_env.get("x"), Some(&42)),
+        match execute(&assign_stmt, &mut env) {
+            Ok(()) => assert_eq!(env.get("x"), Some(&42)),
             Err(s) => assert!(false, "{}", s),
         }
+
+        Ok(())
     }
 
     #[test]
-    fn eval_expression_with_variables() {
+    fn eval_expression_with_variables() -> Result<()> {
         let env = HashMap::from([(String::from("a"), 5), (String::from("b"), 3)]);
         let expr = Expression::Mul(
             Box::new(Expression::Var(String::from("a"))),
@@ -163,11 +196,13 @@ mod tests {
                 Box::new(Expression::CInt(2)),
             )),
         );
-        assert_eq!(eval(&expr, &env), Ok(25));
+        assert_eq!(eval(&expr, &env)?, 25);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_nested_expressions() {
+    fn eval_nested_expressions() -> Result<()> {
         let env = HashMap::new();
         let expr = Expression::Add(
             Box::new(Expression::Mul(
@@ -179,18 +214,20 @@ mod tests {
                 Box::new(Expression::CInt(4)),
             )),
         );
-        assert_eq!(eval(&expr, &env), Ok(12));
+        assert_eq!(eval(&expr, &env)?, 12);
+
+        Ok(())
     }
 
     #[test]
-    fn eval_variable_not_found() {
+    fn eval_variable_not_found() -> Result<()> {
         let env = HashMap::new();
         let var_expr = Expression::Var(String::from("z"));
+        let result = eval(&var_expr, &env);
 
-        assert_eq!(
-            eval(&var_expr, &env),
-            Err(String::from("Variable z not found"))
-        );
+        assert!(result.is_err(), "Variable missing not found");
+
+        Ok(())
     }
 
     #[test]
@@ -207,7 +244,7 @@ mod tests {
          * After executing this program, 'x' must be zero and
          * 'y' must be 55.
          */
-        let env = HashMap::new();
+        let mut env = HashMap::new();
 
         let a1 = Statement::Assignment(Box::new(String::from("x")), Box::new(Expression::CInt(10)));
         let a2 = Statement::Assignment(Box::new(String::from("y")), Box::new(Expression::CInt(0)));
@@ -234,10 +271,10 @@ mod tests {
         let seq2 = Statement::Sequence(Box::new(a2), Box::new(while_statement));
         let program = Statement::Sequence(Box::new(a1), Box::new(seq2));
 
-        match execute(&program, env) {
-            Ok(new_env) => {
-                assert_eq!(new_env.get("y"), Some(&55));
-                assert_eq!(new_env.get("x"), Some(&0));
+        match execute(&program, &mut env) {
+            Ok(()) => {
+                assert_eq!(env.get("y"), Some(&55));
+                assert_eq!(env.get("x"), Some(&0));
             }
             Err(s) => assert!(false, "{}", s),
         }
@@ -256,7 +293,7 @@ mod tests {
          *
          * After executing, 'y' should be 1.
          */
-        let env = HashMap::new();
+        let mut env = HashMap::new();
 
         let condition = Expression::Var(String::from("x"));
         let then_stmt =
@@ -274,8 +311,8 @@ mod tests {
             Statement::Assignment(Box::new(String::from("x")), Box::new(Expression::CInt(10)));
         let program = Statement::Sequence(Box::new(setup_stmt), Box::new(if_statement));
 
-        match execute(&program, env) {
-            Ok(new_env) => assert_eq!(new_env.get("y"), Some(&1)),
+        match execute(&program, &mut env) {
+            Ok(()) => assert_eq!(env.get("y"), Some(&1)),
             Err(s) => assert!(false, "{}", s),
         }
     }
@@ -293,7 +330,7 @@ mod tests {
          *
          * After executing, 'y' should be 7 and 'x' should be 0.
          */
-        let env = HashMap::new();
+        let mut env = HashMap::new();
 
         let a1 = Statement::Assignment(Box::new(String::from("x")), Box::new(Expression::CInt(3)));
         let a2 = Statement::Assignment(Box::new(String::from("y")), Box::new(Expression::CInt(10)));
@@ -320,10 +357,10 @@ mod tests {
             Box::new(Statement::Sequence(Box::new(a2), Box::new(while_statement))),
         );
 
-        match execute(&program, env) {
-            Ok(new_env) => {
-                assert_eq!(new_env.get("y"), Some(&7));
-                assert_eq!(new_env.get("x"), Some(&0));
+        match execute(&program, &mut env) {
+            Ok(()) => {
+                assert_eq!(env.get("y"), Some(&7));
+                assert_eq!(env.get("x"), Some(&0));
             }
             Err(s) => assert!(false, "{}", s),
         }
@@ -345,7 +382,7 @@ mod tests {
          *
          * After executing, 'y' should be 1.
          */
-        let env = HashMap::new();
+        let mut env = HashMap::new();
 
         let inner_then_stmt =
             Statement::Assignment(Box::new(String::from("y")), Box::new(Expression::CInt(1)));
@@ -369,8 +406,8 @@ mod tests {
             Statement::Assignment(Box::new(String::from("x")), Box::new(Expression::CInt(10)));
         let program = Statement::Sequence(Box::new(setup_stmt), Box::new(outer_if_statement));
 
-        match execute(&program, env) {
-            Ok(new_env) => assert_eq!(new_env.get("y"), Some(&1)),
+        match execute(&program, &mut env) {
+            Ok(()) => assert_eq!(env.get("y"), Some(&1)),
             Err(s) => assert!(false, "{}", s),
         }
     }
@@ -386,7 +423,7 @@ mod tests {
          *
          * After executing, 'x' should be 5, 'y' should be 0, and 'z' should be 13.
          */
-        let env = HashMap::new();
+        let mut env = HashMap::new();
 
         let a1 = Statement::Assignment(Box::new(String::from("x")), Box::new(Expression::CInt(5)));
         let a2 = Statement::Assignment(Box::new(String::from("y")), Box::new(Expression::CInt(0)));
@@ -406,11 +443,11 @@ mod tests {
             Box::new(Statement::Sequence(Box::new(a2), Box::new(a3))),
         );
 
-        match execute(&program, env) {
-            Ok(new_env) => {
-                assert_eq!(new_env.get("x"), Some(&5));
-                assert_eq!(new_env.get("y"), Some(&0));
-                assert_eq!(new_env.get("z"), Some(&13));
+        match execute(&program, &mut env) {
+            Ok(()) => {
+                assert_eq!(env.get("x"), Some(&5));
+                assert_eq!(env.get("y"), Some(&0));
+                assert_eq!(env.get("z"), Some(&13));
             }
             Err(s) => assert!(false, "{}", s),
         }
